dfc19b1dec6c3184da989ed603b408ace328f2f9
Alvaro Navarro <alvarowops@MacBook-Pro-de-Alvaro.local>
2024-08-12 19:31:51 -0400

    first commit

---
+++ test_app.py
@@ -0,0 +1,16 @@
+import unittest
+from app import add, subtract
+
+class TestApp(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(10, 5), 15)
+        self.assertEqual(add(-1, 1), 0)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(10, 5), 5)
+        self.assertEqual(subtract(-1, 1), -2)
+
+if __name__ == '__main__':
+    unittest.main()
+
